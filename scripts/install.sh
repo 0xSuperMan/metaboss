31a1f14d0772a83516c7db2fd1599a4eb1c2e6a2
0xSuperMan <zeroxsuperman@proton.me>
2022-09-30 13:28:04 +0530

    Added -f fail parameter to curl and echo download url

--- scripts/install.sh
+++ scripts/install.sh
@@ -52,6 +52,7 @@ case "$PROCESSOR" in
         ;;
 esac
  
+RELEASE_URL="https://github.com/samuelvanderwaal/metaboss/releases/latest/download/"
 BIN="metaboss"
 VERSION="ubuntu-latest"
 
@@ -75,8 +76,11 @@ echo "$(CYN "1.") 🖥  $(CYN "Downloading distribution")"
 echo ""
 
 # downloads the distribution file
-REMOTE="https://github.com/samuelvanderwaal/metaboss/releases/latest/download/"
-curl -L $REMOTE$BIN"-"$DIST --output "$SOURCE/$DIST"
+URL=$RELEASE_URL$BIN"-"$DIST
+echo "Remote URL: $URL"
+echo ""
+
+curl -f -L $URL --output "$SOURCE/$DIST"
 abort_on_error $?
 
 SIZE=$(wc -c "$SOURCE/$DIST" | grep -oE "[0-9]+" | head -n 1)
